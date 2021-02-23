7a0ca8b3bef276e7c59ced529eed8b2dc1ac37d1
susmit <susmitmaji14@gmail.com>
2021-02-23 22:01:21 +0530

    Header navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Let's build the amazon-clone</h1>
+    <div className="app">
+        <Header/>
     </div>
   );
 }
