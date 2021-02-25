a0f3430e6f5621b7b9fda8674d947ce4d5a3d071
susmit <susmitmaji14@gmail.com>
2021-02-25 10:59:14 +0530

    Complete Login Authentication with firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,18 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBe9fTtxdjCriJ8WZ-z1h5VEOkb3v7SsXg",
-    authDomain: "clone-69cd8.firebaseapp.com",
-    projectId: "clone-69cd8",
-    storageBucket: "clone-69cd8.appspot.com",
-    messagingSenderId: "500575618905",
-    appId: "1:500575618905:web:24846809f80588433a6e48",
-    measurementId: "G-0GQ8PDBQ1X"
-  };
+  apiKey: "AIzaSyBe9fTtxdjCriJ8WZ-z1h5VEOkb3v7SsXg",
+  authDomain: "clone-69cd8.firebaseapp.com",
+  projectId: "clone-69cd8",
+  storageBucket: "clone-69cd8.appspot.com",
+  messagingSenderId: "500575618905",
+  appId: "1:500575618905:web:24846809f80588433a6e48",
+  measurementId: "G-0GQ8PDBQ1X",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
